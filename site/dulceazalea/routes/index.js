292caeade2bda6db42855aad7b9855324ff18027
franco fallatti <francofallatti@gmail.com>
2020-08-22 01:44:20 -0300

    modificaicon al agregar producto y implementacion de mainController

--- site/dulceazalea/routes/index.js
+++ site/dulceazalea/routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const controller = require('../controllers/mainController');
+
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('home', { title: 'Dulce Azalea' });
-});
+router.get('/', controller.index);
 
 module.exports = router;
