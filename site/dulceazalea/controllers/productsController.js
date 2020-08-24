625f052736afcdc3f9b48a6d8704b342704a904f
franco fallatti <francofallatti@gmail.com>
2020-08-24 17:15:40 -0300

    implemento ruta /product/create

--- site/dulceazalea/controllers/productsController.js
+++ site/dulceazalea/controllers/productsController.js
@@ -1,17 +1,22 @@
- const database = require("../data/database");
- const fs = require("fs");
- const path = require("path")
+const database = require('../data/database');
+const fs = require('fs');
+const path = require('path');
 
- module.exports = { //exporto un objeto literal con todos los metodos
-    listar: function(req, res) {
-        let productos = database.filter((producto) => {
+module.exports = {
+	//exporto un objeto literal con todos los metodos
+	listar: function (req, res) {
+		let productos = database.filter((producto) => {
 			return producto.id;
 		});
-        res.render('products', {
-                title: "Todos los Productos",
-                productos: database,
-                productos:productos,
-            }) //muestra información de prueba
-    },
- }
- 
+		res.render('products', {
+			title: 'Todos los Productos',
+			productos: database,
+			productos: productos,
+		}); //muestra información de prueba
+	},
+	agregar: function (req, res) {
+		res.render('productAdd', {
+			title: 'Cargar Producto',
+		});
+	},
+};
--- site/dulceazalea/routes/products.js
+++ site/dulceazalea/routes/products.js
@@ -1,13 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const controller = require("../controllers/productsController")
+const controller = require('../controllers/productsController');
 
-router.get("/:d?", controller.listar)//1)Listado de productos/3)Detalle de un producto en particular
-router.get("/create") //2)Formulario de creación de productos
-router.post("/")//4)Acción de creación (a dónde se envía el formulario)
-router.get("/:id/edit")//5)Formulario de edición de productos
-router.put("/:id")//6)Acción de edición (a dónde se envía el formulario)
-router.delete("/:id")//7)Acción de borrado
+router.get('/', controller.listar); //1)Listado de productos/3)Detalle de un producto en particular
+router.get('/create', controller.agregar); //2)Formulario de creación de productos
+router.post('/'); //4)Acción de creación (a dónde se envía el formulario)
+router.get('/:id/edit'); //5)Formulario de edición de productos
+router.put('/:id'); //6)Acción de edición (a dónde se envía el formulario)
+router.delete('/:id'); //7)Acción de borrado
 
-module.exports = router
+module.exports = router;
