d5dd54fc10a71638f36fe88d0bc22a9993b7908b
franco fallatti <francofallatti@gmail.com>
2020-09-01 18:48:23 -0300

    arreglo al guardar imagen en ruta '/create' e implementacion de la ruta 'edit/:id'

--- site/dulceazalea/controllers/productsController.js
+++ site/dulceazalea/controllers/productsController.js
@@ -1,52 +1,74 @@
 const database = require('../data/database');
+const dbCategorias = require('../data/categorias.json');
 const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-	//exporto un objeto literal con todos los metodos
-	listar: function (req, res) {
-		let productos = database.filter((producto) => {
-			return producto.id;
-		});
-		res.render('products', {
-			title: 'Todos los Productos',
-			productos: database,
-			productos: productos,
-		}); //muestra información de prueba
-	},
-	agregar: function (req, res) {
-		// let Categoria = database.filter((producto) => {
-		// 	return producto.category;
-		// });
-		res.render('productAdd', {
-			title: 'Cargar Producto',
-			// categoria: categoria,
-		});
-	},
-	detalle: function (req, res) {
-		let id = req.params.id;
+  //exporto un objeto literal con todos los metodos
+  listar: function (req, res) {
+    res.render('products', {
+      title: 'Todos los Productos',
+      productos: database,
+    }); //muestra información de prueba
+  },
+  agregar: function (req, res) {
+    res.render('productAdd', {
+      title: 'Cargar Producto',
+      categorias: dbCategorias,
+    });
+  },
+  detalle: function (req, res) {
+    let id = req.params.id;
 
-		let producto = database.filter((producto) => {
-			return producto.id == id;
-		});
-		res.render('productDetail', {
-			title: 'Detalle del Producto',
-			id: id,
-			producto: producto[0],
-		});
-	},
-	mostrar: function (req, res) {
-		let id = req.params.id;
-		let productoEdit = database.filter((producto) => {
-			return producto.id == id;
-		});
-		res.render('productEdit', {
-			title: 'Editar Producto',
-			productoEdit: productoEdit[0],
-		});
-	},
-	edit: function (req, res, next) {
-		let id = res.params.id;
-		res.render();
-	},
+    let producto = database.filter((producto) => {
+      return producto.id == id;
+    });
+    res.render('productDetail', {
+      title: 'Detalle del Producto',
+      id: id,
+      producto: producto[0],
+    });
+  },
+  publicar: function (req, res, next) {
+    let lastID = 1;
+    database.forEach(function (producto) {
+      if (producto.id > lastID) {
+        lastID = producto.id;
+      }
+    });
+    let newProduct = {
+      id: lastID + 1,
+      name: req.body.name,
+      description: req.body.description,
+      category: req.body.category,
+      colors: req.body.colors,
+      price: Number(req.body.price),
+      image: req.files[0] ? req.files[0].filename : 'default-image.png',
+    };
+
+    database.push(newProduct);
+
+    fs.writeFileSync(
+      path.join(__dirname, '..', 'data', 'products.json'),
+      JSON.stringify(database),
+      'utf-8'
+    );
+
+    res.redirect('/products');
+  },
+  mostrar: function (req, res) {
+    let id = req.params.id;
+    let productoEdit = database.filter((producto) => {
+      return producto.id == id;
+    });
+    res.render('productEdit', {
+      title: 'Editar Producto',
+      productoEdit: productoEdit[0],
+      CantProducts: database.length,
+    });
+  },
+  edit: function (req, res, next) {
+    let id = res.params.id;
+    res.render();
+  },
 };
--- site/dulceazalea/routes/products.js
+++ site/dulceazalea/routes/products.js
@@ -3,14 +3,18 @@ const router = express.Router();
 
 const controller = require('../controllers/productsController');
 const multer = require('multer');
+const path = require('path');
 
 var storage = multer.diskStorage({
-	destination: function (req, file, cb) {
-		cb(null, '/tmp/my-uploads');
-	},
-	filename: function (req, file, cb) {
-		cb(null, file.fieldname + '-' + Date.now());
-	},
+  destination: function (req, file, cb) {
+    cb(null, 'public/img/products');
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 var upload = multer({ storage: storage });
@@ -18,9 +22,9 @@ var upload = multer({ storage: storage });
 router.get('/', controller.listar); //1)Listado de productos/3)Detalle de un producto en particular
 router.get('/create', controller.agregar); //2)Formulario de creación de productos
 router.get('/:id', controller.detalle); //detalle de producto
-router.post('/create'); //4)Acción de creación (a dónde se envía el formulario)
+router.post('/create', upload.any(), controller.publicar); //4)Acción de creación (a dónde se envía el formulario)
 router.get('/edit/:id', controller.mostrar); //5)Formulario de edición de productos
 router.put('/:id', upload.any(), controller.edit); //6)Acción de edición (a dónde se envía el formulario)
-router.delete('/:id'); //7)Acción de borrado
+router.delete('/delete/:id'); //7)Acción de borrado
 
 module.exports = router;
