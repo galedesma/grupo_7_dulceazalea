c03aa1290f86495a06365c076ce12f0f483da792
francofallatti <66625402+francofallatti@users.noreply.github.com>
2020-09-13 06:20:47 -0300

    register finalizado

--- site/dulceazalea/controllers/usercontroller.js
+++ site/dulceazalea/controllers/usercontroller.js
@@ -1,6 +1,8 @@
 const dbUsers = require('../data/dbUsers');
 const fs = require('fs');
 const path = require('path');
+const bcrypt = require('bcrypt');
+const { check, validationResult, body } = require('express-validator');
 
 module.exports = {
   mostrar_Registro: function (req, res) {
@@ -8,10 +10,42 @@ module.exports = {
       title: 'Registro',
     });
   },
-  ProcessRegister: function (req, res) {},
+  ProcessRegister: function (req, res) {
+    console.log(validationResult(req));
+    let errors = validationResult(req);
+    let LastID = dbUsers.length;
+
+    if (errors.isEmpty()) {
+      let UserNuevo = {
+        id: LastID + 1,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        Email: req.body.Email,
+        password: bcrypt.hashSync(req.body.password, 10),
+        rol: 'user',
+      };
+      dbUsers.push(UserNuevo);
+
+      fs.writeFileSync(
+        path.join(__dirname, '..', 'data', 'users.json'),
+        JSON.stringify(dbUsers),
+        'utf-8'
+      );
+    } else {
+      // if (!errors.isEmpty()) {
+      return res.render('UserRegister', {
+        errors: errors.errors,
+        title: 'Registro',
+      });
+    }
+
+    res.render('UserLogin', {
+      title: 'Ingresar',
+    });
+  },
   mostrar_Login: function (req, res) {
     res.render('UserLogin', {
-      title: 'Login',
+      title: 'UserPerfil',
     });
   },
   ProcessLogin: function (req, res) {},
--- site/dulceazalea/routes/users.js
+++ site/dulceazalea/routes/users.js
@@ -3,15 +3,17 @@ var router = express.Router();
 const controller = require('../controllers/usercontroller');
 const { Router } = require('express');
 
+const RegisterValidator = require('../validators/RegisterValidator');
+
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
 router.get('/register', controller.mostrar_Registro);
-router.post('/register', controller.ProcesRegister);
+router.post('/register', RegisterValidator, controller.ProcessRegister);
 
 router.get('/login', controller.mostrar_Login);
-router.post('login', controller.ProcessLogin);
+// router.post('login', controller.ProcessLogin);
 
 router.get('/profile', controller.profile);
 module.exports = router;
