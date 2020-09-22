e1abea4f57540db8638f99a297ae26e86dda046c
francofallatti <66625402+francofallatti@users.noreply.github.com>
2020-09-22 18:59:40 -0300

    cambios en el perfil

--- site/dulceazalea/routes/products.js
+++ site/dulceazalea/routes/products.js
@@ -10,12 +10,12 @@ router.get('/', controller.listar); //1)Listado de productos/3)Detalle de un pro
 router.get('/create', sessionUserCheck, controller.agregar); //2)Formulario de creación de productos
 router.get('/:id', controller.detalle); //detalle de producto
 router.post(
-    '/create',
-    upImageProduct.any(),
-    productValidator,
-    controller.publicar
-  ); //4)Acción de creación (a dónde se envía el formulario)
-  
+  '/create',
+  upImageProduct.any(),
+  productValidator,
+  controller.publicar
+); //4)Acción de creación (a dónde se envía el formulario)
+
 router.get('/edit/:id', sessionUserCheck, controller.mostrar); //5)Formulario de edición de productos
 router.put('/sent/:id', upImageProduct.any(), controller.edit); //6)Acción de edición (a dónde se envía el formulario)
 router.delete('/delete/:id', controller.eliminar); //7)Acción de borrado
