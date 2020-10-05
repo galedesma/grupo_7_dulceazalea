ec5f23a627e7e1873bf8e394ebc8d2256d4c80c8
francofallatti <66625402+francofallatti@users.noreply.github.com>
2020-10-05 11:34:34 -0300

    instalacion de dependencias y base de datos

--- site/dulceazalea/src/middlewares/upImageProduct.js
+++ site/dulceazalea/src/middlewares/upImageProduct.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 let storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'public/img/users');
+    cb(null, '../public/img/products');
   },
   filename: function (req, file, cb) {
     cb(
---
+++ site/dulceazalea/src/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+const cookieCheck = require('../middlewares/cookieCheck');
+const controller = require('../controllers/mainController');
+
+/* GET home page. */
+router.get('/', cookieCheck, controller.index);
+router.get('/search', controller.search);
+router.get('/carrito', cookieCheck, function (req, res) {
+  res.render('productCart', { title: 'Carrito', usuario: req.session.usuario });
+});
+router.get('/error', function (req, res) {
+  res.render('placeholder', {
+    title: 'Ocurrió un error',
+  });
+}); //Agrego una vista para todo aquello que falta crear
+
+module.exports = router;
